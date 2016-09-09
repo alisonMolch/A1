d5d55ab39fde1bce362dd8d1ad00dd7568ff7ecd
alisonMolch <amolchadsky@gmail.com>
2016-09-09 12:03:47 -0400

    Update A1.java

--- src/A1.java
+++ src/A1.java
@@ -7,25 +7,29 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 
 
 
 public class A1 {
 	public static void main(String[] args){
-		getFiles(args[0]); //try 0
-		//ArrayList<String> arr = makeArrayList(filestr);
-		//addtohash(arr);
+		getFiles("/Users/rentaluser/Documents/workspace/AS1/data_corrected/classification task/atheism/train_docs"); //try 0
+		String result=getFiles("/Users/rentaluser/Documents/workspace/AS1/data_corrected/classification task/atheism/train_docs"); 
+		ArrayList<String> arr = makeArrayList(result);
+		HashMap<String, Float> x= probHash(arr);
+		System.out.println(probability("not", x));
 	}
 	
-	static void getFiles(String path){
+	static String getFiles(String path){
 		File f = new File(path);
 		System.out.println(f.exists());
 		ArrayList<File> files = new ArrayList<File>(Arrays.asList(f.listFiles()));
 	//	System.out.println(files);
-		int y =0;
+		//int y =0;
+		String result="";
 		for (File x :files){
 			try {
-				readFile(x.getPath(), StandardCharsets.US_ASCII);
+				result = result +readFile(x.getPath(), StandardCharsets.US_ASCII);
 				//y=y+1;
 				
 			} catch (IOException e) {
@@ -34,6 +38,9 @@ public class A1 {
 			}
 			
 		}
+		//System.out.println(result);
+		//makeArrayList(result);
+		return result;
 		//System.out.println(y);
 	}
 	
@@ -41,8 +48,9 @@ public class A1 {
 		byte[] encoded = Files.readAllBytes(Paths.get(path));
 		// System.out.println(new String(encoded, encoding));
 		String strfile = new String(encoded, encoding);
-		System.out.println(strProcess(removeHeader(strfile)));
+		//System.out.println(strProcess(removeHeader(strfile)));
 		return strProcess(removeHeader(strfile));
+		//return strProcess(removeHeader(strfile));
 	}
 	
 	static String removeHeader(String file){
@@ -73,39 +81,49 @@ public class A1 {
 	} 
 	
 	static String strProcess(String s){
-		String result = s.replaceAll("[-+^:></\\()|#]", "");
-		String re= result.replaceAll("[.?!]", " [b]");
+		String result = s.replaceAll("[-+^:\"></\\()|#]", "");
+		String re= result.replaceAll("[.?!]", "[b]");
+		re= re.replace(". ", " [b]");
 		return re;
 	}
 	
-	static HashMap<String,Integer> addtohash(ArrayList<String> arr){
-		HashMap<String,Integer> wordcounts = new HashMap<String, Integer>();
+	//creates probability unigram hashtable given an arraylist
+	static HashMap<String,Float> probHash(ArrayList<String> arr){
+		HashMap<String,Float> wordcounts = new HashMap<String, Float>();
 		for(String word: arr){
 			if (wordcounts.containsKey(word)){
-				wordcounts.put(word,wordcounts.get(word)+1);
+				wordcounts.put(word.toLowerCase(),wordcounts.get(word.toLowerCase())+1);
 			}
 			else{
-				wordcounts.put(word,1);
+				wordcounts.put(word.toLowerCase(),(float) 1);
 			}
 		}
+		for(String word: wordcounts.keySet()){
+		
+			wordcounts.put(word.toLowerCase(),wordcounts.get(word.toLowerCase())/arr.size());
+
+		}
+		System.out.println(wordcounts);
 		return wordcounts;
 	}
 	
-	static float probability(String word, HashMap<String,Integer> wordcounts, int length){
-		float count = wordcounts.get(word);
-		return count/length;
+	static float probability(String word, HashMap<String,Float> wordcounts){
+		return wordcounts.get(word);
 	}
 	
 	public static ArrayList<String> makeArrayList(String st){
+		
 		ArrayList<String> myList = new ArrayList<String>(Arrays.asList(st.split("\\s")));
 		myList.removeAll(Arrays.asList("", null));
-		for (String x:myList){
-			if (x.contains("@")){
-				myList.removeAll(Arrays.asList(x, null));
+		Iterator<String> iter = myList.iterator();
+		while (iter.hasNext()){
+			String r = iter.next();
+			if (r.contains("@")){
+				iter.remove();
 			}
 		}
+		System.out.println(myList);
 		return myList;
 	}
-	
 
 } 
