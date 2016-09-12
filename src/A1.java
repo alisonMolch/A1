338d8ba0507d063be7ac48e3c778e39958f91d2a
eliza46 <eliza46@users.noreply.github.com>
2016-09-11 23:06:53 -0400

    Update A1.java

--- src/A1.java
+++ src/A1.java
@@ -15,9 +15,15 @@ import java.util.Map.Entry;
 
 
 public class A1 {
+	/**Main class runs our processor. It reads files from the designated path,
+	 * pre-processes them, and can be used to run our unigram and bigram models.
+	 * @param args
+	 */
 	public static void main(String[] args){
-		getFiles("/Users/rentaluser/Documents/workspace/AS1/data_corrected/classification task/atheism/train_docs"); //try 0
-		String result=getFiles("/Users/rentaluser/Documents/workspace/AS1/data_corrected/classification task/atheism/train_docs/"); 
+		getFiles("Users/elizaweaver/Documents/workspace2110/NLPa1/data_corrected/"
+				+ "classification task/atheism/train_docs");
+		String result=getFiles("Users/elizaweaver/Documents/workspace2110/NLPa1/data_corrected/"
+				+ "classification task/atheism/train_docs/"); 
 		ArrayList<String> arr = makeArrayList(result);
 		//System.out.println(arr.size());
 		HashMap<String, Integer> x= unigramCounts(arr);
@@ -43,68 +49,47 @@ public class A1 {
 		
 	}
 	
-	/** Takes a file path and returns the path as a string */
+	/** Takes a file path and retrieves all the files in that path as a string */
 	static String getFiles(String path){
 		File f = new File(path);
-		//System.out.println(f.exists());
 		ArrayList<File> files = new ArrayList<File>(Arrays.asList(f.listFiles()));
-	//	System.out.println(files);
-		//int y =0;
 		String result="";
 		for (File x :files){
 			try {
 				result = result +readFile(x.getPath(), StandardCharsets.US_ASCII);
-				//y=y+1;
 				
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				//e.printStackTrace();
 			}
 			
 		}
-		//System.out.println(result);
-		//makeArrayList(result);
 		return result;
-		//System.out.println(y);
 	}
 	
 	/**Takes a filepath and a set of characters and reads the files in that path. Returns a processed String
-	 * of file content 
+	 * of file content. 
 	 */
 	static String readFile(String path, Charset encoding) throws IOException{
 		byte[] encoded = Files.readAllBytes(Paths.get(path));
-		// System.out.println(new String(encoded, encoding));
 		String strfile = new String(encoded, encoding);
-		//System.out.println(strProcess(removeHeader(strfile)));
 		return strProcess(removeHeader(strfile));
-		//return strProcess(removeHeader(strfile));
 	}
 	
 	/**Removes the email header of a file */
 	static String removeHeader(String file){
 		int stEmail = 0;
 		if (file.contains("writes")){
-		//	System.out.println("stEmail");
 			stEmail = file.indexOf("writes")+6;
-//			System.out.println("EMAIL");
-			//System.out.println(stEmail);
 		}
 		else if (file.contains("wrote")){
-			//System.out.println("CASEEED");
-//			System.out.println("EMAILEMAIL");
 			stEmail = file.indexOf("wrote")+5;
 		}
 		else{
 			int atindex = file.indexOf('@');
-//			System.out.println("atindex is    " + atindex);
 			String email = file.substring(atindex, file.length());
 			int endEmail = email.indexOf(" ");
-//			System.out.println("endemail is    " + endEmail);
-//	
-//			System.out.println("EMAILEMAILEMAIL");
 			return email.substring(endEmail+1,email.length());
 		}
-
 		return file.substring(stEmail,file.length());
 	} 
 	
@@ -117,9 +102,8 @@ public class A1 {
 	}
 	
 
-	/**Given a string, makes an arraylist where each word is a separate element */
+	/**Given a string, makes an Arraylist where each word is a separate element */
 	public static ArrayList<String> makeArrayList(String st){
-		
 		ArrayList<String> myList = new ArrayList<String>(Arrays.asList(st.split("\\s")));
 		myList.removeAll(Arrays.asList("", null));
 		Iterator<String> iter = myList.iterator();
@@ -133,47 +117,31 @@ public class A1 {
 		return myList;
 	}
 
+	/**Given an arraylist of words, returns a hashmap within a hashmap of the counts 
+	 * of each combination of 2 words*/
 	public static HashMap<String, HashMap<String, Integer>> bigramCounts(ArrayList<String> arr){
-		HashMap<String, HashMap<String, Integer>> outerHash = new HashMap<String,HashMap<String, Integer>>();
+		HashMap<String, HashMap<String, Integer>> outerHash = 
+				new HashMap<String,HashMap<String, Integer>>();
 		for(int i=0; i<arr.size()-2; i++){
 			if (outerHash.containsKey(arr.get(i))){
-				
 				HashMap<String,Integer>innerhash = outerHash.get(arr.get(i));
-				
 				if (innerhash.containsKey(arr.get(i+1))){
-//					try{
-						//System.out.println(arr.get(i+1));
-						String x = arr.get(i+1);
-						innerhash.put(arr.get(i+1), innerhash.get(arr.get(i+1))+1);
-					
-					
-//					catch (NullPointerException e){
-//						
-//					}
-					
+					innerhash.put(arr.get(i+1), innerhash.get(arr.get(i+1))+1);	
 				}
 				else{
-					//System.out.println(arr.get(i+1));
 					innerhash.put(arr.get(i+1), 1);
 				}
-				//System.out.println(innerhash);
 			}
 			else{
-				//System.out.println(arr.get(i));
 				HashMap<String,Integer> innerhash = new HashMap<String, Integer>();
 				innerhash.put(arr.get(i+1),1);
 				outerHash.put(arr.get(i), innerhash);
 			}
-			
 		}
 		return outerHash;
 	}
 	
-//	public static HashMap<String, HashMap<String, Integer>> bigramCounts1(ArrayList<String> arr){
-//		HashMap<String, HashMap<String, Integer>> outerHash = new HashMap<String,HashMap<String, Integer>>();
-//		
-//		
-//	}
+	/**Given an arraylist of words, returns a hashmap of the counts of each word*/
 	public static HashMap<String, Integer> unigramCounts(ArrayList<String> arr){
 		HashMap<String, Integer> result = new HashMap<String, Integer>();
 		for (String x:arr){
@@ -187,39 +155,38 @@ public class A1 {
 		return result;
 	}
 	
-	
-	public static HashMap<String, HashMap<String, Float>> bigramProbHashmap(HashMap<String, HashMap<String, Integer>> map, HashMap<String, Integer> unigramCounts){
-		HashMap<String, HashMap<String, Float>> outer = new HashMap<String, HashMap<String, Float>>(); 
+	/**Given a hashmap of words and their counts, return a hashmap of 
+	 * pairs of words and their probabilities using a bigram model*/
+	public static HashMap<String, HashMap<String, Float>> bigramProbHashmap
+	(HashMap<String, HashMap<String, Integer>> map, HashMap<String, Integer> unigramCounts){
+		HashMap<String, HashMap<String, Float>> outer = 
+				new HashMap<String, HashMap<String, Float>>(); 
 		for (Entry<String, HashMap<String, Integer>> entry : map.entrySet()){
 			
 			Integer countOuter = unigramCounts.get(entry.getKey());
 			HashMap<String, Float> inner = new HashMap<String, Float>();
 			for (Entry<String, Integer> innerEntry: map.get(entry.getKey()).entrySet()){
-				
 				Integer x = map.get(entry.getKey()).get(innerEntry.getKey());
 				float prob = (float) x/ (float) countOuter;
 				inner.put(innerEntry.getKey(), prob);
-				
 			}
 			outer.put(entry.getKey(), inner);	
-			
 		}
 		return outer;
 	}
 	
-	/*gives the bigram probability of string a given string b
-	 * 
-	 */
+	/**Gives the bigram probability of string a given string b */
 	public static float bigram(String a, String b, HashMap<String, HashMap<String, Float>> map){
 		try{
 			return map.get(b).get(a);
 		}
-		catch (NullPointerException e){
-			
+		catch (NullPointerException e){	
 		}
 		return (float) 0;
 	}
 	
+	/**Given a hashmap of words and counts, returns a hashmap of 
+	 * words and probabilities using a unigram model.*/
 	public static HashMap<String, Float> unigramProbHashmap(HashMap<String, Integer> map){
 		HashMap<String, Float> result = new HashMap<String, Float>();
 		Integer sum = 0;
@@ -236,24 +203,19 @@ public class A1 {
 		return result;
 	}
 	
+	/** Given a hashmap of probabilities and a word, returns the probability
+	 * of that word */
 	public static float unigram(String x, HashMap<String, Float> map){
 		return map.get(x);
 	}
 	
-
-	public static ArrayList<String> sorted(ArrayList<String> arr){
-		Collections.sort(arr);
-		return arr;
-	}
-	
+	/** Returns a random sentence using the unigram model*/
 	public static StringBuffer unigramRandomSentence(String sentence, ArrayList<String> arr){
 		boolean flag = true;
 		StringBuffer result= new StringBuffer();
 		if (!sentence.equals("")){
 			result= result.append(sentence+ " ");
 		}
-		
-
 		while(flag){
 			double rand = Math.random();
 			int i = arr.size();
@@ -267,15 +229,17 @@ public class A1 {
 			}
 			else{
 				result = result.append(arr.get(x)+" ");
-			}
-			
+			}	
 		}
 		return result;
 	}
 	
-	public static HashMap<String, LinkedHashMap<String, Float>> cumProb(HashMap<String, HashMap<String, Float>> map){
-		
-		HashMap<String, LinkedHashMap<String, Float>> outer = new HashMap<String, LinkedHashMap<String, Float>>();
+	/** Returns a hashtable of words and linked hashtables of words and their cumulative
+	 * probabilities, which is used in sentence generation*/
+	public static HashMap<String, LinkedHashMap<String, Float>> 
+	cumProb(HashMap<String, HashMap<String, Float>> map){
+		HashMap<String, LinkedHashMap<String, Float>> outer = 
+				new HashMap<String, LinkedHashMap<String, Float>>();
 		for (Entry <String,HashMap<String, Float>> m :map.entrySet()){
 			float sum = 0;
 			HashMap<String, Float> innerOg = map.get(m.getKey());
@@ -291,6 +255,8 @@ public class A1 {
 		return outer;
 	}
 	
+	/** Given a word, uses the cumulative probabilities from 
+	 * the hashamp to return the next word*/
 	public static String nextWord(String prev, HashMap<String, LinkedHashMap<String, Float>> map){
 		double random = Math.random();
 		//System.out.println(random);
@@ -306,7 +272,9 @@ public class A1 {
 		return next;
 	}
 	
-	public static String genSentenceBigram(String sentence, HashMap<String, LinkedHashMap<String, Float>> cumBiprobz){
+	/** Generates a sentence using the bigram model*/
+	public static String genSentenceBigram(String sentence, HashMap<String, 
+			LinkedHashMap<String, Float>> cumBiprobz){
 		StringBuffer result = new StringBuffer();
 		String word = lastWord(sentence);
 		boolean flag=true;
@@ -324,22 +292,22 @@ public class A1 {
 			}
 			else{
 				result.append(word + " ");
-				
 			}
 		}
 		return result.toString();
 	}
 	
+	/** Returns the last word in a sentence*/
 	public static String lastWord(String sentence){
 		int x = sentence.lastIndexOf(" ");
 		if (x==-1){
 			return sentence;
 		}
-
 		else{
 			return sentence.substring(x+1, sentence.length());
 		}
 	}
+}
 	
 	
 } 
